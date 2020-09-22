69f301fc6d36dbe1904c55c6892760184ce562b8
Fabricio Palavecino <fabricioleonelpalavecino@gmail.com>
2020-09-22 11:44:47 -0300

    first commit

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,46 +1,21 @@
-import { createConnection, getConnectionOptions, ConnectionOptions } from "typeorm";
-import dotenv from 'dotenv';
-dotenv.config();
-
+import { createConnection } from "typeorm";
 import path from "path";
 
 export async function connect() {
-    const getOptions = async () => {
-        let connectionOptions: ConnectionOptions;
-        connectionOptions = {
-          type: "mysql",
-          host: "localhost",
-          port: 3306,
-          username: "root",
-          password: "",
-          database: "challenge_puzzle_graphql",
-          entities: [
-              path.join(__dirname, "../entity/**/**.ts")
-          ],
-          synchronize: true
-        };
-        if (process.env.DATABASE_URL) {
-          Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
-        } else {
-          // gets your default configuration
-          // you could get a specific config by name getConnectionOptions('production')
-          // or getConnectionOptions(process.env.NODE_ENV)
-          connectionOptions = await getConnectionOptions(); 
-        }
-      
-        return connectionOptions;
-      };
-      
-      const connect2Database = async (): Promise<void> => {
-          const typeormconfig = await getOptions();
-          await createConnection(typeormconfig);
-      };
-      
-      connect2Database().then(async () => {
-          console.log('Connected to database');
-      });
+    await createConnection({
+        type: "mysql",
+        host: process.env.BD_HOST,
+        port: 3306,
+        username: "root",
+        password: "",
+        database: "challenge_puzzle_graphql",
+        entities: [
+            path.join(__dirname, "../entity/**/**.ts")
+        ],
+        synchronize: true
+
+    });
+    console.log("Database is connected");
 }
-
-
 
 
